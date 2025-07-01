aeaf312f704dcfff066f1b74fc7a07d8266014a6
Eugene Fairley <ecfairley@ucdavis.edu>
2025-06-30 20:37:18 -0700

    'save'

--- comfy_extras/nodes_wan.py
+++ comfy_extras/nodes_wan.py
@@ -37,6 +37,7 @@ class WanImageToVideo:
             image = torch.ones((length, height, width, start_image.shape[-1]), device=start_image.device, dtype=start_image.dtype) * 0.5
             image[:start_image.shape[0]] = start_image
 
+            print('start_image:', start_image)
             concat_latent_image = vae.encode(image[:, :, :, :3])
             mask = torch.ones((1, 1, latent.shape[2], concat_latent_image.shape[-2], concat_latent_image.shape[-1]), device=start_image.device, dtype=start_image.dtype)
             mask[:, :, :((start_image.shape[0] - 1) // 4) + 1] = 0.0
