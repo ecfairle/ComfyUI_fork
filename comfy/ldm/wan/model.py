83ce2a1073f977cf56ac6db17380df358ecd91c7
Eugene Fairley <ecfairley@ucdavis.edu>
2025-07-02 14:30:38 -0700

    save

--- comfy/ldm/wan/model.py
+++ comfy/ldm/wan/model.py
@@ -507,8 +507,8 @@ class WanModel(torch.nn.Module):
         grid_sizes = torch.stack(
             [torch.tensor(u.shape[2:], dtype=torch.long) for u in x])
         x = [u.flatten(2).transpose(1, 2) for u in x]
-        seq_len = seq_lens.max()
         seq_lens = torch.tensor([u.size(1) for u in x], dtype=torch.long)
+        seq_len = seq_lens.max()
         assert seq_lens.max() <= seq_len
         x = torch.cat([
             torch.cat([u, u.new_zeros(1, seq_len - u.size(1), u.size(2))],
