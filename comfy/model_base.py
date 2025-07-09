7aac3a1a86d0b1b0bf46ba2e1b5723482ae24b91
Eugene Fairley <ecfairley@ucdavis.edu>
2025-07-09 10:28:54 -0700

    dtype

--- comfy/model_base.py
+++ comfy/model_base.py
@@ -1227,7 +1227,7 @@ class WAN21(BaseModel):
 
         tracks = kwargs.get("tracks", None)
         if tracks is not None:
-            res = patch_motion(tracks.to(device), res[0], 220.0, (4, 16), 2)[None]
+            res = patch_motion(tracks.to(device), res[0], 220.0, (4, 16), 2)[None].to(res.dtype)
 
         return res
 
