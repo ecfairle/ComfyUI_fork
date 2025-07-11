9a21ef24bedb63d6db11e924671c10bd99931622
Eugene Fairley <ecfairley@ucdavis.edu>
2025-07-11 09:52:35 -0700

    save

--- comfy_extras/nodes_wan.py
+++ comfy_extras/nodes_wan.py
@@ -570,63 +570,65 @@ class WanTrackToVideo:
         tracks_data = parse_json_tracks(tracks)
         latent = torch.zeros([batch_size, 16, ((length - 1) // 4) + 1, height // 8, width // 8], 
                            device=comfy.model_management.intermediate_device())
-        if tracks_data:
-            # Convert tracks to tensor format
-            arrs = []
-            for i, track in enumerate(tracks_data):
-                pts = pad_pts(track)
-                arrs.append(pts)
+        if not tracks_data:
+            return WanImageToVideo().encode(positive, negative, vae, width, height, length, batch_size, start_image=start_image, clip_vision_output=clip_vision_output)
+        
+        # Convert tracks to tensor format
+        arrs = []
+        for i, track in enumerate(tracks_data):
+            pts = pad_pts(track)
+            arrs.append(pts)
 
-            tracks_np = np.stack(arrs, axis=0)
-            processed_tracks = process_tracks(tracks_np, (width, height), length - 1).unsqueeze(0)
+        tracks_np = np.stack(arrs, axis=0)
+        processed_tracks = process_tracks(tracks_np, (width, height), length - 1).unsqueeze(0)
+        
+        if start_image is not None:
+            start_image = comfy.utils.common_upscale(start_image[:length].movedim(-1, 1), width, height, "bilinear", "center").movedim(1, -1)
             
-            if start_image is not None:
-                start_image = comfy.utils.common_upscale(start_image[:length].movedim(-1, 1), width, height, "bilinear", "center").movedim(1, -1)
-                
-                lat_h = height // 8
-                lat_w = width // 8
+            lat_h = height // 8
+            lat_w = width // 8
 
-                msk = torch.ones(1, latent.shape[2], lat_h, lat_w, device=start_image.device)
-                msk[:, 1:] = 0
-                
-                # repeat first frame 4 times
-                msk = torch.concat([
-                    torch.repeat_interleave(msk[:, 0:1], repeats=4, dim=1), msk[:, 1:]
+            msk = torch.ones(1, latent.shape[2], lat_h, lat_w, device=start_image.device)
+            msk[:, 1:] = 0
+            
+            # repeat first frame 4 times
+            msk = torch.concat([
+                torch.repeat_interleave(msk[:, 0:1], repeats=4, dim=1), msk[:, 1:]
+            ],
+                dim=1)
+
+            # Reshape mask into groups of 4 frames
+            msk = msk.view(1, msk.shape[1] // 4, 4, lat_h, lat_w)
+
+            # first batch
+            msk = msk.transpose(1, 2)
+
+            dummy_frames = torch.ones(3, length - 1, height, width) * .5
+            
+            start_image = start_image.permute(3,0,1,2) # C, T, H, W
+            res = torch.concat([
+                    start_image.to(start_image.device),
+                    dummy_frames
                 ],
-                    dim=1)
+                    dim=1).to(start_image.device)
 
-                # Reshape mask into groups of 4 frames
-                msk = msk.view(1, msk.shape[1] // 4, 4, lat_h, lat_w)
-
-                # first batch
-                msk = msk.transpose(1, 2)
-
-                dummy_frames = torch.ones(3, length - 1, height, width) * .5
-                
-                start_image = start_image.permute(3,0,1,2) # C, T, H, W
-                res = torch.concat([
-                        start_image.to(start_image.device),
-                        dummy_frames
-                    ],
-                        dim=1).to(start_image.device)
-
-                res = res.permute(1,2,3,0)[:, :, :, :3]  # T, H, W, C
-                
-                y = vae.encode(res)
-                
-                # Add motion features to conditioning
-                positive = node_helpers.conditioning_set_values(positive,
-                                                                {"tracks": processed_tracks,
-                                                                 "concat_mask": msk,
-                                                                "concat_latent_image": y,
-                                                                "ati_temperature": temperature,
-                                                                 "ati_topk": topk})
-                negative = node_helpers.conditioning_set_values(negative, 
-                                                                {"tracks": processed_tracks,
-                                                                 "concat_mask": msk,
-                                                                "concat_latent_image": y,
-                                                                "ati_temperature": temperature,
-                                                                 "ati_topk": topk})
+            res = res.permute(1,2,3,0)[:, :, :, :3]  # T, H, W, C
+            
+            y = vae.encode(res)
+            
+            # Add motion features to conditioning
+            positive = node_helpers.conditioning_set_values(positive,
+                                                            {"tracks": processed_tracks,
+                                                                "concat_mask": msk,
+                                                            "concat_latent_image": y,
+                                                            "ati_temperature": temperature,
+                                                                "ati_topk": topk})
+            negative = node_helpers.conditioning_set_values(negative, 
+                                                            {"tracks": processed_tracks,
+                                                                "concat_mask": msk,
+                                                            "concat_latent_image": y,
+                                                            "ati_temperature": temperature,
+                                                                "ati_topk": topk})
                 
 
         # Handle clip vision output if provided
